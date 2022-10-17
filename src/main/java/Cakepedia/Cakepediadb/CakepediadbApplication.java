e43b5eba9be48d77ec48a6445072638339886433
cindynrh <cindynurhabibah@gmail.com>
2022-10-17 17:47:12 +0700

    first commit

---
+++ src/main/java/Cakepedia/Cakepediadb/CakepediadbApplication.java
@@ -0,0 +1,13 @@
+package Cakepedia.Cakepediadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakepediadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakepediadbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cakepedia/Cakepediadb/CakepediadbApplicationTests.java
@@ -0,0 +1,13 @@
+package Cakepedia.Cakepediadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakepediadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
